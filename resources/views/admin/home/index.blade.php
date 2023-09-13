57b470bb00d23e9a887c32e865d5b7cd90e176bf
i_ilyukha <ilia.ilyukha@gmail.com>
2023-09-13 23:23:02 +0300

    Init commit

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin_layout')
+
+@section('title', 'Главная')
+
+@section('name', 'Главная')
+
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+     
+        <!-- /.row -->
+        <!-- Main row -->
+       
+        <!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+</section>
+@endsection;
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\admin\HomeController;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\RouteCompiler;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['role:admin'])->prefix('admin_panel')->group(function () {
+    Route::get('/', [App\Http\Controllers\admin\HomeController::class, 'index']);
+});
+
